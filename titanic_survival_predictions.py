5698731af07174446291c6cdab3de0f17fe02567
Max Parker <Max.parker1@outlook.com>
2021-09-25 17:25:30 +0100

    cleaning up code

--- titanic_survival_predictions.py
+++ titanic_survival_predictions.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing, model_selection, linear_model
+from enum import Enum, auto
+
+
+class DataType(Enum):
+    DATASET = auto()
 
 
 class Titanic:
@@ -17,7 +22,26 @@ class Titanic:
         self.predictions: list = []
         self.accuracy: float = 0
 
-    def preparing_data(self):
+    def missing_values(self) -> dict:
+        """
+        Obtain the number of missing values in each column.
+        """
+
+        column_missing_values = {}
+
+        for column in self.dataset:
+
+            column_missing_values[column] = self.dataset[column].isna(
+            ).sum()
+
+        print("Number of missing values in each column -> {}".format(column_missing_values))
+
+        return column_missing_values
+
+    def preparing_data(self) -> DataType.DATASET:
+        """
+        Preparing dataset so that the model can train on it properly.
+        """
 
         self.dataset = self.dataset.drop(
             columns=["PassengerId", "Ticket", "Cabin", "Embarked", "SibSp", "Parch", "Name"])
@@ -26,9 +50,8 @@ class Titanic:
 
         """
         Encoding Sex Variable:
-
-            Female -> 1
-            Male -> 0
+            Female: 1
+            Male: 0
         """
         for i, row in self.dataset.iterrows():
             if self.dataset["Sex"][i] == "female":
@@ -39,6 +62,10 @@ class Titanic:
         return self.dataset
 
     def running_model(self) -> float:
+        """
+        Obtain accuracy of prediction model on Titanic Dataset.
+        """
+
         self.X = self.dataset.iloc[:, 1:]
         self.y = self.dataset.loc[:, "Survived"]
 
@@ -63,6 +90,30 @@ class Titanic:
 
         return self.accuracy
 
+    def column_class_distribution(self, column: list) -> dict:
+        """
+        Obtain frequency and percentage of each occurence in a particular column of the dataset.
+        """
+
+        track_frequencies = {}
+        track_percentages = {}
+
+        for row in column:
+            if row not in track_frequencies.keys():
+                track_frequencies[row] = 1
+            else:
+                track_frequencies[row] += 1
+
+        print("Frequency of each value: {}".format(track_frequencies))
+
+        for key, value in track_frequencies.items():
+            track_percentages[str(key) +
+                              "_percentage"] = (value / len(column)) * 100
+
+        print("Percentage of each value of total: {}".format(track_percentages))
+
+        return track_frequencies, track_percentages
+
     def __str__(self):
         return(str(self.dataset))
 
@@ -71,5 +122,7 @@ if __name__ == "__main__":
     training_data = pd.read_csv("train.csv")
 
     dataset = Titanic(training_data)
+    dataset.missing_values()
     dataset.preparing_data()
+    dataset.column_class_distribution(training_data["Sex"])
     dataset.running_model()
